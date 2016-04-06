9c8e97fd1fb725370b005d3ee23f1f603e89ade0
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-04-06 16:30:34 +0200

    rename ussd_registration -> ussdapp.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
             src: {
                 app: {
                     smsapp: 'src/smsapp.js',
-                    ussd_registration: 'src/ussd_registration.js'
+                    ussdapp: 'src/ussdapp.js'
                 },
                 smsapp: [
                     'src/index.js',
@@ -17,11 +17,11 @@ module.exports = function (grunt) {
                     '<%= paths.src.app.smsapp %>',
                     'src/init.js'
                 ],
-                ussd_registration: [
+                ussdapp: [
                     'src/index.js',
                     'src/utils.js',
                     'src/utils_project.js',
-                    '<%= paths.src.app.ussd_registration %>',
+                    '<%= paths.src.app.ussdapp %>',
                     'src/init.js'
                 ],
                 all: [
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
             },
             dest: {
                 smsapp: 'go-app-sms.js',
-                ussd_registration: 'go-app-ussd_registration.js'
+                ussdapp: 'go-app-ussdapp.js'
             },
             test: {
                 smsapp: [
@@ -39,12 +39,12 @@ module.exports = function (grunt) {
                     '<%= paths.src.app.smsapp %>',
                     'test/smsapp.test.js'
                 ],
-                ussd_registration: [
+                ussdapp: [
                     'test/setup.js',
                     'src/utils.js',
                     'src/utils_project.js',
-                    '<%= paths.src.app.ussd_registration %>',
-                    'test/ussd_registration.test.js'
+                    '<%= paths.src.app.ussdapp %>',
+                    'test/ussdapp.test.js'
                 ]
             }
         },
@@ -82,9 +82,9 @@ module.exports = function (grunt) {
                 src: ['<%= paths.src.smsapp %>'],
                 dest: '<%= paths.dest.smsapp %>'
             },
-            ussd_registration: {
-                src: ['<%= paths.src.ussd_registration %>'],
-                dest: '<%= paths.dest.ussd_registration %>'
+            ussdapp: {
+                src: ['<%= paths.src.ussdapp %>'],
+                dest: '<%= paths.dest.ussdapp %>'
             }
 
         },
@@ -96,8 +96,8 @@ module.exports = function (grunt) {
             test_smsapp: {
                 src: ['<%= paths.test.smsapp %>']
             },
-            test_ussd_registration: {
-                src: ['<%= paths.test.ussd_registration %>']
+            test_ussdapp: {
+                src: ['<%= paths.test.ussdapp %>']
             }
         }
     });
